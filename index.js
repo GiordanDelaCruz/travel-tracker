506fb9dc93162926367a3c45bd6216a1c604dcea
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2024-02-16 19:15:06 -0500

    Added error handling for invalid and previously countries.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-const db =  new pg.Client({
+const db = new pg.Client({
   user: "postgres", 
   host: "localhost", 
   database: "world", 
@@ -22,72 +22,75 @@ db.connect();   // Connect to database
 /******          Custom Functions                     ******/
 /***********************************************************/
 async function checkVisited(){
+  // Get information of all visited countries in a javascript object
   const result = await db.query("SELECT country_code FROM visited_countries");
 
   let countries = [];
   result.rows.forEach( (country) => {
     countries.push(country.country_code);
   });
+
+  return countries;
 }
 
 /***********************************************************/
 /******             Route Handling                    ******/
 /***********************************************************/
-// Get Homepage
+// GET Homepage
 app.get("/", async (req, res) => {
-  //Write your code here.
   try {
-
-    // Get information of all visited countries in a javascript object
-    const result = await db.query(
-      "SELECT country_code FROM visited_countries"
-    );
-
     // Get ONLY the country codes 
-    let codeArr = [];
-    for(let i = 0; i < result.rows.length; i++){   
-      codeArr.push(result.rows[i].country_code);
-    }
-
-    // db.end();  // Close database for good pratice
+    let countries = await checkVisited()
     
     // Render homepage
     res.render("index.ejs", {
-      countries: codeArr, 
-      total:  codeArr.length, 
+      countries: countries, 
+      total:  countries.length, 
     })
   } catch (err) {
     console.error("Error executing query", err.stack);
   }
 });
 
-// Handle POST request of new visited country
+// INSERT new country
 app.post("/add", async (req, res) => {
   try {
-    // Get user input
     const input = req.body.country;
 
     // Search for country code 
-    const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [input]);
-    console.log(result);
-
-    // Add country to visited list 
-    if( result.rows.length !== 0){
-      db.query("INSERT INTO visited_countries(country_code) VALUES($1)", [result.rows[0].country_code])
-    }else{
-      console.log("Country code not found.");
-
-      // Invalid Country
-      // req.session.error = 'Country does not exist, please try again.';
-      // res.redirect("/");
-    }
-    // Redirect to homepage
-    res.redirect("/");
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1", 
+      [input]
+    );
     
-  } catch (error) {
-    // Repeated Country 
-    // req.session.error = 'Country has already been added, please try again.';
-    // res.redirect("/");
+    const data = result.rows[0];
+    const countryCode = data.country_code;  
+    try {
+      // Add country to visited list 
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)", 
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+        console.log(err);
+         // Repeated Country 
+        let countries = await checkVisited();
+        res.render("index.ejs", {
+          countries: countries, 
+          total:  countries.length, 
+          error: 'Country has already been added, please try again.'
+        });
+    }
+  } catch (err) {
+    console.log(err);
+    // Invalid Country
+    let countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: countries, 
+      total:  countries.length, 
+      error: 'Country does not exist, please try again.'
+    });
   }  
 });
 
